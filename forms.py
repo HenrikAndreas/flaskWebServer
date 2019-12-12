047364ef2e4a3ac6eeb4731389bd8c27f5a6f097
Henrik <henrikbratli@live.no>
2019-12-12 14:38:18 +0100

    Added login system. Not yet functional

---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+# This python code is being translated into HTML code, that replaces the usual HTML Input / submit fields
+class Registration(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
+    
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    SubmitField = SubmitField('Sign Up')
+    
+class Login(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
+    
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    SubmitField = SubmitField('Login')
---
+++ run.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, url_for, escape, request, flash, redirect
+from forms import Registration, Login # From another python file
+
+posts = [
+    {
+        'title' : 'Welcome',
+        'author' : 'Henrik Andreas',
+        'date' : '11.12.2019',
+        'content' : 'Here you are able to control parts of my Raspberry Pi, simply because why the fuck not? This is a page with a lot of bullshit, so I hope you will enjoy!'
+        },
+    {
+        'title' : 'Instructions',
+        'author' : 'Henrik Andreas',
+        'date' : '11.12.2019',
+        'content' : 'If you access the "Playground" section you can choose to turn off my lamp!'
+    }
+]
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pakhoda'
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html", posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = Login() # Form from the forms.py file
+    if form.validate_on_submit():
+        if form.username.data == "Henrik" and form.password.data == "7mammA99":
+            return redirect(url_for('home'))
+        else:
+            flash(f"Unsuccessful login. Check credentials", "danger")
+    return render_template('login.html', title = 'Login', form=form)
+
+
+
+#DRAFITNG!!
+@app.route('/register')
+def register():
+    form = Registration()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/users')
+def users():
+    return render_template('users.html')
+
+@app.route('/users/<username>')
+def showUser(username):
+    return f"{escape(username)}'s profile"
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
+
+
